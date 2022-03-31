2345ef89314a98b33787881b85698f50917d8998
jeevan thapa <jthapa01@outlook.com>
2022-03-30 22:23:53 -0400

    Add data access for remaining microservices

--- CloudWeather.Temperature/Program.cs
+++ CloudWeather.Temperature/Program.cs
@@ -1,3 +1,14 @@
+using CloudWeather.Temperature.DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TemperatureDbContext>(
+    opts =>
+    {
+        opts.EnableSensitiveDataLogging();
+        opts.EnableDetailedErrors();
+        opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient
+);
 var app = builder.Build();
 app.Run();
