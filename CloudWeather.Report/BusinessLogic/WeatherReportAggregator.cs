e26a0154c0154ff0ee911877fac3d76f3a394afe
jeevan thapa <jthapa01@outlook.com>
2022-03-31 15:07:05 -0400

    Add WeatherReport microservice Logic

---
+++ CloudWeather.Report/BusinessLogic/WeatherReportAggregator.cs
@@ -0,0 +1,135 @@
+﻿using CloudWeather.Report.Config;
+using CloudWeather.Report.DataAccess;
+using CloudWeather.Report.Models;
+using Microsoft.Extensions.Options;
+using System.Text.Json;
+
+namespace CloudWeather.Report.BusinessLogic
+{
+    /// <summary>
+    /// Aggregates data from multiple external sources to build a weather report
+    /// </summary>
+    public interface IWeatherReportAggregator
+    {
+        /// <summary>
+        /// Builds and returns a Weekly Weather Report.
+        /// Persists WeeklyWeatherReport data
+        /// </summary>
+        /// <param name="zip"></param>
+        /// <param name="days"></param>
+        /// <returns></returns>
+        public Task<WeatherReport> BuildReport(string zip, int days);
+    }
+    public class WeatherReportAggregator : IWeatherReportAggregator
+    {
+        private readonly IHttpClientFactory _http;
+        private readonly ILogger<WeatherReportAggregator> _logger;
+        private readonly WeatherDataConfig _weatherDataConfig;
+        private readonly WeatherReportDbContext _db;
+
+        public WeatherReportAggregator(
+            IHttpClientFactory http,
+            ILogger<WeatherReportAggregator> logger,
+            IOptions<WeatherDataConfig> weatherConfig,
+            WeatherReportDbContext db)
+        {
+            _http = http;
+            _logger = logger;
+            _weatherDataConfig = weatherConfig.Value;
+            _db = db;
+        }
+
+        public async Task<WeatherReport> BuildReport(string zip, int days)
+        {
+            var httpClient  = _http.CreateClient();
+            var precipData = await FetchPrecipitaionData(httpClient, zip, days);
+            var totalSnow = GetTotalSnow(precipData);
+            var totalRain = GetTotalRain(precipData);
+            _logger.LogInformation(
+                $"zip: {zip} over last {days} days: " +
+                $"total snow: {totalSnow}, rain: {totalRain}"
+            );
+            var tempData = await FetchTemperatureData(httpClient, zip, days);
+            var averageHighTemp = tempData.Average(t => t.TempHigh);
+            var averageLowTemp = tempData.Average(t=>t.TempLow);
+            _logger.LogInformation(
+                $"zip: {zip} over last {days} days: " +
+                $"lo temp: {averageLowTemp}, hi temp: {averageHighTemp}"
+            );
+            var weeatherReport = new WeatherReport
+            {
+                AverageHighF = Math.Round(averageHighTemp, 1),
+                AverageLowF = Math.Round(averageLowTemp, 1),
+                RainFallTotalInches = totalRain,
+                SnowTotalInches = totalSnow,
+                ZipCode = zip,
+                CreatedOn = DateTime.UtcNow,
+            };
+            // TODO: Use 'cached' weather reports instead of making round trips when possible?
+            _db.Add(weeatherReport); // tracks changes
+            await _db.SaveChangesAsync(); 
+            return weeatherReport;
+        }
+
+        private static decimal GetTotalRain(List<PrecipitationModel> precipData)
+        {
+            var totalRain = precipData
+                .Where(p => p.WeatherType == "rain")
+                .Sum(p=>p.AmountInches);
+            return Math.Round(totalRain, 1);
+        }
+
+        private static decimal GetTotalSnow(List<PrecipitationModel> precipData)
+        {
+            var totalSnow = precipData
+                .Where(p => p.WeatherType == "snow")
+                .Sum(p => p.AmountInches);
+            return Math.Round(totalSnow, 1);
+        }
+
+        private async Task<List<TemperatureModel>> FetchTemperatureData(HttpClient httpClient, string zip, int days)
+        {
+            var endpoint = BuildTemperatureServiceEndpoint(zip,days);
+            var temperatureRecords = await httpClient.GetAsync(endpoint);
+            var jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            var temperatureData = await temperatureRecords
+                .Content
+                .ReadFromJsonAsync<List<TemperatureModel>>(jsonSerializerOptions);
+            return temperatureData?? new List<TemperatureModel>();
+        }
+
+        private async Task<List<PrecipitationModel>> FetchPrecipitaionData(HttpClient httpClient, string zip, int days)
+        {
+            var endpoint = BuildPrecipitationEndpoint(zip, days);
+            var precipRecords = await httpClient.GetAsync(endpoint);
+            var jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            var precipData = await precipRecords
+                .Content
+                .ReadFromJsonAsync<List<PrecipitationModel>>(jsonSerializerOptions);
+            return precipData ?? new List<PrecipitationModel>();
+        }
+
+        private string BuildTemperatureServiceEndpoint(string zip, int days)
+        {
+            var tempServiceProtocol = _weatherDataConfig.TempDataProtocol;
+            var tempServiceHost = _weatherDataConfig.TempDataHost;
+            var tempServicePort = _weatherDataConfig.TempDataPort;
+            return $"{tempServiceProtocol}://{tempServiceHost}:{tempServicePort}/observation/{zip}?days={days}";
+        }
+        private string BuildPrecipitationEndpoint(string zip, int days)
+        {
+            var precipServiceProtocol = _weatherDataConfig.PrecipDataProtocol;
+            var precipServiceHost = _weatherDataConfig.PrecipDataHost;
+            var precipServicePort = _weatherDataConfig.PrecipDataPort;
+            return $"{precipServiceProtocol}://{precipServiceHost}:{precipServicePort}/observation/{zip}?days={days}";
+        }
+    }
+}
--- CloudWeather.Report/Program.cs
+++ CloudWeather.Report/Program.cs
@@ -1,7 +1,17 @@
+using CloudWeather.Report.BusinessLogic;
+using CloudWeather.Report.Config;
 using CloudWeather.Report.DataAccess;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHttpClient();
+// Method call lifescope
+builder.Services.AddTransient<IWeatherReportAggregator, WeatherReportAggregator>();
+builder.Services.AddOptions();
+builder.Services.Configure<WeatherDataConfig>(builder.Configuration.GetSection("WeatherDataConfig"));
+
+
 builder.Services.AddDbContext<WeatherReportDbContext>(
     opts =>
     {
@@ -11,4 +21,11 @@ builder.Services.AddDbContext<WeatherReportDbContext>(
     }, ServiceLifetime.Transient
 );
 var app = builder.Build();
+
+app.MapGet(
+    "/weather-report/{zip}"
+    , async (string zip, [FromQuery] int? days, IWeatherReportAggregator weatherAgg) =>
+     {
+
+     });
 app.Run();
